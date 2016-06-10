ea46cb94d2a2913fade7a4d8c43bebf5d7b3da2f
alexey olenev <aleksey.olenev@inn.ru>
2016-06-10 14:17:04 +0300

    naive basic authentication

--- src/main/scala/eu/inn/authentication/AuthenticationService.scala
+++ src/main/scala/eu/inn/authentication/AuthenticationService.scala
@@ -1,26 +1,54 @@
 package eu.inn.authentication
 
 import com.typesafe.config.Config
+import eu.inn.binders.value.Null
 import eu.inn.hyperbus.Hyperbus
 import eu.inn.hyperbus.model._
 import eu.inn.hyperbus.transport.api.matchers.{RequestMatcher, Specific}
 import eu.inn.hyperbus.transport.api.uri.Uri
 
+import scala.collection.JavaConversions._
 import scala.concurrent.{ExecutionContext, Future}
 
 class AuthenticationService(hyperbus: Hyperbus, config: Config)(implicit executionContext: ExecutionContext) {
 
   val requestMatcher = RequestMatcher(Some(Uri("/auth")), Map(Header.METHOD → Specific(Method.GET)))
   hyperbus.onCommand(requestMatcher) { request: DynamicRequest ⇒
-      Future {
-        authenticate(request.body.content.credentials.asString) match {
-          case Some(authUser) ⇒ AuthenticationResponse(AuthenticationResponseBody(authUser))
-          case None ⇒ NotFound(ErrorBody("not-found"))
-        }
+    Future {
+      authenticate(request.body.content.credentials.asString) match {
+        case Some(authUser) ⇒
+          Ok(AuthenticationResponseBody(authUser))
+
+        case None ⇒
+          NotFound(ErrorBody("not-found"))
       }
+    }
   }
 
-  def authenticate(credentials: String): Option[AuthUser] = {
-    None
+  def authenticate(authString: String): Option[AuthUser] = {
+    var authUser: Option[AuthUser] = None
+    val schemeAndCredentials = authString.replace("Authorization: ", "")
+    if (isBasic(schemeAndCredentials)) {
+      val credentials = schemeAndCredentials.toLowerCase.replace("basic ", "").split(":")
+      val login = credentials(0)
+      val password = credentials(1)
+      getBasicAuthConfigs.foreach { authConfig ⇒
+        if (authConfig.hasPath(login)) {
+          val userConfig = authConfig.getConfig(login)
+          if (userConfig.getString("password") == password) {
+            val id = userConfig.getString("id")
+            val roles = userConfig.getStringList("roles").toSet
+            authUser = Some(AuthUser(id, roles, Null))
+          }
+        }
+      }
+    }
+    authUser
   }
+
+  def isBasic(credentials: String): Boolean = {
+    credentials.toLowerCase.startsWith("basic")
+  }
+
+  def getBasicAuthConfigs: Set[Config] = config.getConfigList("auth-service.basic").toSet
 }
