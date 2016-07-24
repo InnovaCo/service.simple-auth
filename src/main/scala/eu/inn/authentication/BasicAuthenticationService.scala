40f0e1c5a0dcf9342fad58f4ff4f86385ec1e161
alexey olenev <aleksey.olenev@inn.ru>
2016-07-24 20:27:44 +0300

    use compile-time type checking for hyperbus requests

--- src/main/scala/eu/inn/authentication/BasicAuthenticationService.scala
+++ src/main/scala/eu/inn/authentication/BasicAuthenticationService.scala
@@ -4,8 +4,6 @@ import com.typesafe.config.Config
 import eu.inn.binders.value.Null
 import eu.inn.hyperbus.Hyperbus
 import eu.inn.hyperbus.model._
-import eu.inn.hyperbus.transport.api.matchers.{RequestMatcher, Specific}
-import eu.inn.hyperbus.transport.api.uri.Uri
 
 import scala.collection.JavaConversions._
 import scala.concurrent.{ExecutionContext, Future}
@@ -13,10 +11,9 @@ import scala.concurrent.{ExecutionContext, Future}
 class BasicAuthenticationService(hyperbus: Hyperbus, config: Config)
                                 (implicit executionContext: ExecutionContext) {
 
-  val requestMatcher = RequestMatcher(Some(Uri("/auth")), Map(Header.METHOD → Specific(Method.GET)))
-  hyperbus.onCommand(requestMatcher) { request: DynamicRequest ⇒
+  hyperbus ~> { request: AuthenticationRequest ⇒
     Future {
-      authenticate(request.body.content.credentials.asString) match {
+      authenticate(request.body.credentials) match {
         case Some(authUser) ⇒
           Ok(AuthenticationResponseBody(authUser))
 
